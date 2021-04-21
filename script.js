3556dc04f2352c3dc9c83abf8798e42ba905e65b
umar <unzippinglinux@gmail.com>
2021-04-21 08:37:53 +0600

    small improvement

--- script.js
+++ script.js
@@ -13,8 +13,10 @@ addTaskBtn.addEventListener("click", () => {
     alert("Cant leave input field empty!!!");
   } else {
     // make a list item
-    const li = `<li>${inputField.value}</li>`;
-
+    const li = `<li><p>${
+      inputField.value
+    }</p><button onclick=${"randomF(event)"} class="btn btn-primary ms-4">Delete</button>
+    </li>`;
     // add items on the list
     listOfTasks.innerHTML = listOfTasks.innerHTML + li;
     inputField.value = "";
@@ -22,7 +24,7 @@ addTaskBtn.addEventListener("click", () => {
 });
 
 // Execute a function when the user releases a key on the keyboard
-inputField.addEventListener("keyup", (event) => {
+inputField.addEventListener("keyup", event => {
   // trigger search on clicking enter
   if (event.key === "Enter") {
     // Trigger the button element with a click
@@ -30,8 +32,6 @@ inputField.addEventListener("keyup", (event) => {
   }
 });
 
-listOfTasks.addEventListener("click", (e) => {
-  e.target.parentNode.removeChild(e.target);
-});
-
-//Need to add event propagation to stop unwanted action
+const randomF = e => {
+  e.target.parentNode.remove();
+};
